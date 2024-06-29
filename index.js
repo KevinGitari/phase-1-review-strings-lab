7fe71211fc8d435a3e66535db19c80d3204236a7
KevinGitari <Kevin>
2024-06-29 19:07:55 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+//==> string
+
+ welcomeMessage = 'Welcome to Flatbook, ';
+ function
+ welcomeMessage(){
+    welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+ }
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const shortGreeting = '';
+function 
+ shortGreeting(){
+    const shortGreeting = 'Welcome, ';
+}
+//`.toUpperCase()`
+const currentUser = 'Grace Hopper';
+
+function
+const welcomeMessage(){
+    const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+    const excitedWelcomeMessage = welcomeMessage.toUpperCase();  
+}
+
+function
+const currentUser = Grace Hopper(){ 
+    currentUser.slice(0, 1);
+}
+
+const shortGreeting = `Welcome,`;(){
+    function
+    const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+}
+
+
+
+
+
+
+
+
+
